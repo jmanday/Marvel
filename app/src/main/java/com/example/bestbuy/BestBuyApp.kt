6d7cb021685b677ac7e77ed17e16cd07235b3a50
jesusgarciamanday <jmanday@gmail.com>
2020-10-10 16:23:58 +0200

    [feature/viewmodel-product-list] More refactor.

--- app/src/main/java/com/example/bestbuy/BestBuyApp.kt
+++ app/src/main/java/com/example/bestbuy/BestBuyApp.kt
@@ -5,14 +5,14 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class BestBuy : Application() {
+class BestBuyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         startKoin{
             androidLogger()
-            androidContext(this@BestBuy)
+            androidContext(this@BestBuyApp)
         }
     }
 }
--- app/src/main/java/com/example/bestbuy/ui/ProductListFragment.kt
+++ app/src/main/java/com/example/bestbuy/ui/ProductListFragment.kt
@@ -1,4 +1,4 @@
-package com.example.bestbuy
+package com.example.bestbuy.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment

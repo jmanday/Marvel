4f6c293e56b0c548bbdc9586c3e91044f1d14193
jesusgarciamanday <jmanday@gmail.com>
2020-10-10 16:33:29 +0200

    [feature/viewmodel-product-list] New BaseFragment added.

---
+++ app/src/main/java/com/example/bestbuy/ui/fragments/BaseFragment.kt
@@ -0,0 +1,15 @@
+package com.example.bestbuy.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    protected abstract fun initialize()
+}

3d55f7150e16672e949f6d7482ebe5b6aa11497a
jesusgarciamanday <jmanday@gmail.com>
2020-10-11 13:04:41 +0200

    [feature/product-list-adapter] ProductAdapter added.

---
+++ app/src/main/java/com/example/bestbuy/ui/adapters/ProductAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.bestbuy.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bestbuy.R
+import com.example.bestbuy.ui.viewholder.ProductViewHolder
+import com.example.core_domain.Product
+import com.manday.coreui.adapter.BaseAdapter
+
+
+class ProductAdapter(listProduct: List<Product>, listener: (Product, View) -> Unit) : BaseAdapter<Product, ProductViewHolder>(listProduct, listener) {
+
+    override fun generateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view_item_product, parent, false)
+
+        return ProductViewHolder(v)
+    }
+}

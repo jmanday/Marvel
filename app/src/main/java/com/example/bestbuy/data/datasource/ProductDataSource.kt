e78c0fbd4543f588654e78d727252ea7ff7588f0
jesusgarciamanday <jmanday@gmail.com>
2020-10-11 11:37:38 +0200

    [feature/mapper-product] Refactor.

--- app/src/main/java/com/example/bestbuy/data/datasource/ProductDataSource.kt
+++ app/src/main/java/com/example/bestbuy/data/datasource/ProductDataSource.kt
@@ -1,8 +1,9 @@
 package com.example.bestbuy.data.datasource
 
 import androidx.lifecycle.LiveData
+import com.example.bestbuy.data.ProductEntity
 
 interface ProductDataSource {
 
-    fun getProductList(): LiveData<List<String>?>
+    fun getProductList(): LiveData<List<ProductEntity>?>
 }

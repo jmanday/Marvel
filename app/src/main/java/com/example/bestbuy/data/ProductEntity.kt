50d8e011b73a572eb5f9751b49661ba303e0ad9e
jesusgarciamanday <jmanday@gmail.com>
2020-10-11 11:36:00 +0200

    [feature/mapper-product] Properties nullables in ProductEntity.

--- app/src/main/java/com/example/bestbuy/data/ProductEntity.kt
+++ app/src/main/java/com/example/bestbuy/data/ProductEntity.kt
@@ -8,23 +8,23 @@ data class ProductEntity(
     var id: Int? = null,
 
     @SerializedName("name")
-    var name: String,
+    var name: String? = null,
 
     @SerializedName("brand")
-    var brand: String,
+    var brand: String? = null,
 
     @SerializedName("price")
-    var price: Int,
+    var price: Int? = null,
 
     @SerializedName("currency")
-    var currency: String,
+    var currency: String? = null,
 
     @SerializedName("image")
-    var image: String,
+    var image: String? = null,
 
     @SerializedName("_link")
-    var _link: String,
+    var _link: String? = null,
 
     @SerializedName("_type")
-    var _type: String
+    var _type: String? = null
 )

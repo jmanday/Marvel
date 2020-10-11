c2b6dec36fb1c7444bb30298fb8b47ab25832bba
jesusgarciamanday <jmanday@gmail.com>
2020-10-11 10:47:52 +0200

    [feature/product-repository] Repository added in ProductViewModel.

--- app/src/main/java/com/example/bestbuy/ui/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/example/bestbuy/ui/viewmodels/ProductViewModel.kt
@@ -3,12 +3,18 @@ package com.example.bestbuy.ui.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.bestbuy.data.ProductEntity
+import com.example.bestbuy.data.datasource.RemoteProductDataSource
+import com.example.bestbuy.repository.ProductRepository
+import com.example.bestbuy.repository.ProductRepositoryImpl
+import org.koin.java.KoinJavaComponent.inject
 
 class ProductViewModel: ViewModel() {
 
+    //private val productRepository: ProductRepository by inject(ProductRepository::class.java)
+    private val productRepository: ProductRepository = ProductRepositoryImpl(RemoteProductDataSource())
     var products = MutableLiveData<List<ProductEntity>>()
 
-    init {
-
+    fun getProducts() {
+        productRepository.getProducts()
     }
 }

ff39ca486247145fdf232545a734b27c27ae964b
jesusgarciamanday <jmanday@gmail.com>
2020-10-12 18:00:31 +0200

    [feature/request-product-detail] New method added in the api in order to get the details of a product by id.

--- app/src/main/java/com/example/bestbuy/data/datasource/net/ProductApi.kt
+++ app/src/main/java/com/example/bestbuy/data/datasource/net/ProductApi.kt
@@ -1,11 +1,17 @@
 package com.example.bestbuy.data.datasource.net
 
+import com.example.bestbuy.data.ProductDetailEntity
 import com.example.bestbuy.data.ProductResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductApi {
 
     @GET("products/")
     fun getProducts(): Call<ProductResponse>
+
+    @GET("products/{id}")
+    fun getProductsById(@Path("id") id: Int?): Call<ProductDetailEntity>
+
 }

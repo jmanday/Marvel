972150f12adc0231f321bf5bfd5270a16a0a2d8a
jesusgarciamanday <jmanday@gmail.com>
2020-10-12 12:59:09 +0200

    [feature/fragment-detail-product] Class Product converted to Serializable.

--- core-domain/src/main/java/com/example/core_domain/Product.kt
+++ core-domain/src/main/java/com/example/core_domain/Product.kt
@@ -1,8 +1,10 @@
 package com.example.core_domain
 
+import java.io.Serializable
+
 data class Product (
     var id: Int?,
+    var name: String?,
     var image: String?,
-    var price: Int?,
     var selected: Boolean = false
-)
+) : Serializable

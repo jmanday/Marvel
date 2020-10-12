704f21641876f0c07d0842a3a4e5e74edb81eb8b
jesusgarciamanday <jmanday@gmail.com>
2020-10-12 16:28:13 +0200

    [feature/motion-navigation] Factory for NavigateFromProductToDetailFragment added in module di.

--- app/src/main/java/com/example/bestbuy/di/module.kt
+++ app/src/main/java/com/example/bestbuy/di/module.kt
@@ -2,6 +2,7 @@ package com.example.bestbuy.di
 
 import com.example.bestbuy.data.datasource.ProductDataSource
 import com.example.bestbuy.data.datasource.RemoteProductDataSource
+import com.example.bestbuy.navigation.NavigateFromProductToDetailFragment
 import com.example.bestbuy.repository.ProductRepositoryImpl
 import com.example.bestbuy.repository.ProductRepository
 import org.koin.dsl.module
@@ -12,4 +13,6 @@ val appModuleDependencies = module {
     single<ProductDataSource> { RemoteProductDataSource() }
 
     single<ProductRepository> { ProductRepositoryImpl(get()) }
+
+    factory { NavigateFromProductToDetailFragment() }
 }

11ecae29caef897f042c79a170a8ecd5d790c31b
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2022-07-28 15:39:44 +0200

    [feature-60/adding-flow] Adding StateFlow into the ViewModels insteadof LiveData

--- app/src/main/java/com/example/bestbuy/ui/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/example/bestbuy/ui/viewmodels/ProductViewModel.kt
@@ -2,30 +2,40 @@ package com.example.bestbuy.ui.viewmodels
 
 import androidx.lifecycle.*
 import com.example.bestbuy.repository.ProductRepository
-import com.example.core_data.utils.ExecutorViewModel
 import com.example.core_domain.Product
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.inject
 
-class ProductViewModel: ExecutorViewModel() {
+class ProductViewModel: ViewModel() {
 
     private val productRepository: ProductRepository by inject(ProductRepository::class.java)
-    private lateinit var _products: MutableLiveData<List<Product>>
+    private val _state = MutableStateFlow(UIState())
+    val state: StateFlow<UIState> = _state.asStateFlow()
 
-    val products: LiveData<List<Product>>
-        get() {
-            if (!::_products.isInitialized) {
-                _products = MutableLiveData()
-                viewModelScope.launch {
-                    productRepository.getProducts().collect {
-                        _products.value = it
-                    }
-                }
-            }
+    init {
+        refresh()
+    }
 
-            return _products
+    private fun refresh() {
+        viewModelScope.launch {
+            _state.value = UIState(loading = true)
+            _state.value = UIState(products = productRepository.getProducts().first())
         }
+    }
 
-
+    data class UIState(
+         val loading: Boolean = false,
+         val products: List<Product>? = null,
+         val navigateTo: Product? = null
+    )
 }
+
+/*
+@Suppress("UNCHECKED_CAST")
+class ProductViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ProductViewModel() as T
+    }
+}
+ */

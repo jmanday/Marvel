a36ffbbe5f31dff5628d405168e0fa3cc8e8826a
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2022-08-06 17:55:14 +0200

    [feature-73/updating-ui-layer] ProductListFragment and ProductListViewModel updated.

--- app/src/main/java/com/example/bestbuy/ui/fragments/ProductListFragment.kt
+++ app/src/main/java/com/example/bestbuy/ui/fragments/ProductListFragment.kt
@@ -1,23 +1,21 @@
 package com.example.bestbuy.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.*
+import com.example.bestbuy.R
 import com.example.bestbuy.databinding.FragmentProductListBinding
 import com.example.bestbuy.navigation.NavigateFromProductToDetailFragment
 import com.example.bestbuy.ui.adapters.ProductAdapter
-import com.example.bestbuy.ui.viewmodels.ProductViewModel
+import com.example.bestbuy.ui.viewmodels.ProductListViewModel
 import com.example.core_domain.Product
 import com.manday.coredata.navigation.MotionNavigate
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.inject
 
 
-class ProductListFragment : BaseFragment() {
+class ProductListFragment : BaseFragment(R.layout.fragment_product_list) {
 
-    private lateinit var fragmentProductListBinding: FragmentProductListBinding
     private var adapter: ProductAdapter
     /*
     private val vieModel: ProductViewModel by viewModels {
@@ -25,8 +23,8 @@ class ProductListFragment : BaseFragment() {
     }
      */
 
-    private val vieModel: ProductViewModel by lazy {
-        ViewModelProvider(this).get(ProductViewModel::class.java)
+    private val vieModel: ProductListViewModel by lazy {
+        ViewModelProvider(this).get(ProductListViewModel::class.java)
     }
 
     private val navigateToDetailFragment: MotionNavigate<Product> by inject(
@@ -43,30 +41,28 @@ class ProductListFragment : BaseFragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        fragmentProductListBinding = FragmentProductListBinding.inflate(inflater)
-        fragmentProductListBinding.productRecyclerView.adapter = adapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentProductListBinding.bind(view)
+        binding.productRecyclerView.adapter = adapter
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                vieModel.state.collect(::updateUI)
+                vieModel.state.collect { binding.updateUI(it) }
             }
         }
-
-        return fragmentProductListBinding.root
     }
 
-    private fun updateUI(state: ProductViewModel.UIState) {
-        fragmentProductListBinding.progress.visibility = if (state.loading) View.VISIBLE else View.GONE
-        state.products?.let { adapter.load(it) }
-    }
 
+    /*
     override fun initialize() {
         mToolBar = fragmentProductListBinding.toolbar
+    }
+
+     */
+
+    private fun FragmentProductListBinding.updateUI(state: ProductListViewModel.UIProductListState) {
+        progress.visibility = if (state.loading) View.VISIBLE else View.GONE
+        state.products?.let { adapter.load(it) }
     }
 }

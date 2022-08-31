30a13985fc14d135a1f8683582c4bef21ed928ce
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2022-08-31 17:42:10 +0200

    [feature-73/updating-ui-layer] using the id layout for setContentView

--- app/src/main/java/com/example/bestbuy/ui/common/MainActivity.kt
+++ app/src/main/java/com/example/bestbuy/ui/common/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bestbuy.ui.common
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import com.example.bestbuy.R
 import com.example.bestbuy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(ActivityMainBinding.inflate(layoutInflater).root)
+        setContentView(R.layout.activity_main)
     }
 
 }

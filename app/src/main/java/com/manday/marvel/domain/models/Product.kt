828372d3342d5bfed984ae2b9a708093069ac7db
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-02-14 12:50:03 +0100

    [feature-102/adding_room_orm] Removing old code

--- app/src/main/java/com/manday/marvel/domain/models/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.manday.marvel.domain.models
-
-data class Product (
-    var id: Int?,
-    var name: String?,
-    var image: String?,
-    var selected: Boolean = false
-)
--- app/src/main/java/com/manday/marvel/ui/models/ProductModel.kt
+++
@@ -1,8 +0,0 @@
-package com.manday.marvel.ui.models
-
-import java.io.Serializable
-
-data class ProductModel(
-    val id: Int? = null,
-    val imagePath: String? = null
-) : Serializable

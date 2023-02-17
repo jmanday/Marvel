cb73193f2bc286db1597473b794c4e3c2a9c05ca
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-02-17 11:06:18 +0100

    [feature-102/adding_room_orm] Logic to save on local

--- app/src/main/java/com/manday/marvel/domain/repository/InternalCharacterRepository.kt
+++ app/src/main/java/com/manday/marvel/domain/repository/InternalCharacterRepository.kt
@@ -1,8 +1,13 @@
 package com.manday.marvel.domain.repository
 
+import android.util.Log
 import com.manday.marvel.BuildConfig
+import com.manday.marvel.MarvelCharactersApp
 import com.manday.marvel.data.datasource.LocalDataSource
 import com.manday.marvel.data.datasource.NetDataSource
+import com.manday.marvel.data.datasource.db.models.toListCharacterEntity
+import com.manday.marvel.data.datasource.db.room.RoomDataSource
+import com.manday.marvel.data.datasource.net.models.toListMarvelCharacter
 import com.manday.marvel.domain.mD5Provider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -11,29 +16,26 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class InternalCharacterRepository @Inject constructor(
-    private val netNetDataSource: NetDataSource
+    private val netNetDataSource: NetDataSource,
+    private val localDataSource: LocalDataSource
 ) : CharacterRepository {
-
-    /*
-    override suspend fun getProducts(): Flow<List<Product>?> {
-        return flow {
-            if (localDataSource.getProducts().isEmpty()) {
-                val products = netNetDataSource.getProductList()?.map { it.toLocalProduct() }
-                products?.let {
-                    localDataSource.saveProducts(it)
-                }
-            }
-
-            emit(localDataSource.getProducts().map { it.toProduct() })
-        }.flowOn(Dispatchers.IO)
-    }
-*/
 
     override suspend fun getCharacters(): Flow<CharacterResult> {
         return flow {
-            val callResult = netNetDataSource.getCharacters(mD5Provider.getMD5(BuildConfig.HASH_KEY))
-            val result = if (callResult.isNullOrEmpty()) CharacterResult.WrongResult else CharacterResult.SuccessfullResult(
-                callResult.filter { !it.thumbnailPath.contains("image_not_available".toRegex()) })
+            lateinit var result: CharacterResult
+            val localMarvelCharacter = localDataSource.getCharacters()
+
+            result = if (localMarvelCharacter.isEmpty()) {
+                val callResult = netNetDataSource.getCharacters(mD5Provider.getMD5(BuildConfig.HASH_KEY))
+                if (callResult.isNullOrEmpty()) CharacterResult.WrongResult else {
+                    val marvelCharacterList = callResult.filter { !it.thumbnailPath.contains("image_not_available".toRegex()) }
+                    localDataSource.saveCharacters(marvelCharacterList.toListMarvelCharacter())
+                    CharacterResult.SuccessfullResult(marvelCharacterList)
+                }
+            } else {
+                CharacterResult.SuccessfullResult(localMarvelCharacter.toListCharacterEntity())
+            }
+
             emit(result)
         }.flowOn(Dispatchers.IO)
     }

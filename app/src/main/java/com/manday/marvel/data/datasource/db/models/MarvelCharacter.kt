742feec80682a00d3d1daa6720cd8b149fd2f938
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-02-17 11:04:31 +0100

    [feature-102/adding_room_orm] Methods to map between entities

--- app/src/main/java/com/manday/marvel/data/datasource/db/models/MarvelCharacter.kt
+++ app/src/main/java/com/manday/marvel/data/datasource/db/models/MarvelCharacter.kt
@@ -3,11 +3,20 @@ package com.manday.marvel.data.datasource.db.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.manday.marvel.data.datasource.net.models.CharacterEntity
 
 @Entity(tableName = "marvel_character_table")
 data class MarvelCharacter(
-    @PrimaryKey val id: Int,
-    @ColumnInfo(name = "name") val name: String?,
-    @ColumnInfo(name = "description") val email: String?,
-    @ColumnInfo(name = "thumbnailPath") val avatar: String?
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "name") val name: String? = null,
+    @ColumnInfo(name = "description") val description: String? = null,
+    @ColumnInfo(name = "thumbnailPath") val avatar: String? = null
 )
+
+fun List<MarvelCharacter>.toListCharacterEntity() = this.map {
+    CharacterEntity(
+        name = it.name ?: "",
+        description = it.description ?: "",
+        thumbnailPath = it.avatar ?: ""
+    )
+}
--- app/src/main/java/com/manday/marvel/data/datasource/net/models/CharacterEntity.kt
+++ app/src/main/java/com/manday/marvel/data/datasource/net/models/CharacterEntity.kt
@@ -1,6 +1,7 @@
 package com.manday.marvel.data.datasource.net.models
 
 import android.os.Parcelable
+import com.manday.marvel.data.datasource.db.models.MarvelCharacter
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -9,3 +10,18 @@ data class CharacterEntity(
     val description: String,
     val thumbnailPath: String
 ): Parcelable
+
+fun CharacterEntity.toMarveCharacter() =
+    MarvelCharacter(
+        name = this.name,
+        description = this.description,
+        avatar = this.thumbnailPath
+    )
+
+fun List<CharacterEntity>.toListMarvelCharacter() = this.map {
+    MarvelCharacter(
+        name = it.name,
+        description = it.description,
+        avatar = it.thumbnailPath
+    )
+}

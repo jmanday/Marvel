7684c3d7a1add202df5b1248ecfbabc8b0e06fbc
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-02-17 11:06:31 +0100

    [feature-102/adding_room_orm] suspend modifier added

--- app/src/main/java/com/manday/marvel/data/datasource/db/room/CharacterDao.kt
+++ app/src/main/java/com/manday/marvel/data/datasource/db/room/CharacterDao.kt
@@ -3,6 +3,7 @@ package com.manday.marvel.data.datasource.db.room
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.manday.marvel.data.datasource.db.models.MarvelCharacter
 
@@ -10,11 +11,11 @@ import com.manday.marvel.data.datasource.db.models.MarvelCharacter
 interface CharacterDao {
 
     @Query("SELECT * FROM marvel_character_table")
-    fun getAll(): List<MarvelCharacter>
+    suspend fun getAll(): List<MarvelCharacter>
 
-    @Insert
-    fun insertAll(characters: List<MarvelCharacter>)
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(characters: List<MarvelCharacter>)
 
     @Delete
-    fun delete(character: MarvelCharacter)
+    suspend fun delete(character: MarvelCharacter)
 }

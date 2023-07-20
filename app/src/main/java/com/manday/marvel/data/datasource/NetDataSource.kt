e4910fd562a014cb65d238451215a49d5c5aa40c
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-07-20 15:42:17 +0200

    [feature-111/Update_test_Hilt] minor change

--- app/src/main/java/com/manday/marvel/data/datasource/NetDataSource.kt
+++ app/src/main/java/com/manday/marvel/data/datasource/NetDataSource.kt
@@ -1,7 +1,6 @@
 package com.manday.marvel.data.datasource
 
 import com.manday.marvel.data.datasource.net.models.CharacterEntity
-import com.manday.marvel.data.datasource.net.models.CharactersData
 
 interface NetDataSource {
     suspend fun getCharacters(hashCode: String): List<CharacterEntity>

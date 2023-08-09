992064e11a8f2b25b96d54f59e992286cabf6956
Jesus Garcia Manday <jesus.garcia@productmadness.com>
2023-08-09 16:27:05 +0200

    [feature-111/Update_test_Hilt] updated dependencies

--- domain/character/build.gradle.kts
+++ domain/character/build.gradle.kts
@@ -34,7 +34,22 @@ android {
 }
 
 dependencies {
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    KotlinxDependencies.apply {
+        implementation(kotlinCoroutines)
+    }
+
+    AndroidxDependencies.apply {
+        implementation(coreKtx)
+    }
+
+    DIDependencies.apply {
+        implementation(javaxInject)
+    }
+
+    TestDependencies.apply {
+        testImplementation(junit)
+        testImplementation(mockk)
+        testImplementation(coroutinesTest)
+    }
+
 }
